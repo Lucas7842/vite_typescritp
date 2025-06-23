4fb5611e4314fc2b4803ee6222a9479a76294730
Lucas Martins <lucasmartins1096@hotmail.com>
2025-06-22 23:15:22 -0300

    Foi adicionado mais coisas

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
-export {};
+
+
+import _ from 'lodash';
+import clipboard from 'clipboard';
+
+const button = new Clipboard('button');
+
+function handleCopy(event:any){
+  console.log(event);
+}
+
+button.on('success', handleCopy);
+
+console.log(_.difference([1,45], [2,1,3]));
+
 
 interface Produto{
   nome: string;
